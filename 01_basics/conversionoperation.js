4657ba2e5fa25fe192f6ebfa9b7300404237225f
Abhiwak28 <102908843+Abhiwak28@users.noreply.github.com>
2023-10-01 06:16:07 +0000

    conversion of data typess

---
+++ 01_basics/conversionoperation.js
@@ -0,0 +1,21 @@
+let score = undefined
+
+console.log(typeof score);
+
+let valueinnumber = Number (score)
+
+console.log(typeof valueinnumber);
+console.log(valueinnumber);
+
+// "33=33
+// "33abc" =>nan
+//true => 1 , false =>0
+
+let isloggedin = 1
+let booleanisloggedin = Boolean(isloggedin)
+console.log(booleanisloggedin);
+
+let somenumber = 33
+let stringnumber = String(somenumber)
+console.log(stringnumber);
+console.log(typeof stringnumber);
