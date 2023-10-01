f1d6869bd6c7a7022d9f655c61078e3ca9628ccd
Abhiwak28 <102908843+Abhiwak28@users.noreply.github.com>
2023-10-01 05:59:48 +0000

    datatypes

---
+++ 01_basics/const let .js
@@ -0,0 +1,16 @@
+const accountID = 144453
+let accountemail = "abhiwk9@goggl.com"
+var accountPassword = '1234'
+let accountState;
+
+// /*prefer not to use var because of issue in block and functional scope */
+
+accountCity = "Jaipur"
+
+// accountID = 2 //not allowed 
+acountemail = "abhiwk9@goggl.com"
+var accountPassword = '678'
+accountCity = "indore"
+
+console.log(accountID);
+console.table([accountID,accountPassword,accountCity,accountState])
---
+++ 01_basics/datatypes.js
@@ -0,0 +1,26 @@
+"use strict;" //treat all js code as newer version
+
+// alert(3+3)
+// WE ARE USING NODE JS NOT BROWSER
+console.log(3+3)
+console.log("abhiwak")
+
+let name = "abhiwak"
+let age =18
+let isloggedin = false
+let state = null
+
+// **primitive data types
+// number => 2 to power 53
+// bigint
+// string
+// boolean tf
+// null (stand alone value)
+// undefined =>
+// symbol(react,figma,uniqueness)
+
+// object
+console.log(typeof"");
+console.log(typeof age);
+console.log(typeof null); //object
+console.log(typeof undefined);
